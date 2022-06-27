31edc20b733ced5fdfa83f60465feade88435159
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-27 12:16:59 -0400

    Add autosize to Textarea

--- resources/views/components/textarea.vue
+++ resources/views/components/textarea.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+import autosize from "autosize";
+
 interface Props {
     label?: string;
     placeholder?: string;
+    rows?: string | number;
 }
 
-const { label, placeholder } = defineProps<Props>();
+const { label, rows = 3, placeholder } = defineProps<Props>();
+
+const textarea = ref<HTMLInputElement | null>(null);
+
+onMounted(() => autosize(textarea.value!));
 </script>
 
 <template>
     <div class="field">
         <label v-if="!!label" class="label">{{ label }}</label>
         <div class="control">
-            <textarea class="textarea" v-bind="{ placeholder }" />
+            <textarea
+                class="textarea"
+                ref="textarea"
+                v-bind="{ rows, placeholder }"
+            />
         </div>
     </div>
 </template>
