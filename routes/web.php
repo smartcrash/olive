f2fcc2ac391e1fa8cd04b58cd8ccc84ca0916c0c
Diego Da'Silva <smartcrashomg@gmail.com>
2022-06-27 20:53:56 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return inertia('index');
-});
+Route::get('/', IndexController::class);
 Route::post('/comments', [CommentController::class, 'store']);
